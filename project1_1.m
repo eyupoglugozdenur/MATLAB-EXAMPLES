159e7a0bd68013c8ae8aa8b794bc59a22b97b2bf
gözdenur <81879623+eyupoglugozdenur@users.noreply.github.com>
2024-05-27 00:08:34 +0300

    MATLAB

--- project1_1.m
+++ project1_1.m
@@ -6,15 +6,15 @@ averageOfMonthlyCall=input("Please Enter the Average of Monthly Phone Call in Mi
 averageOfMonthlyCall=averageOfMonthlyCall-200; %Because the First 200 Minutes Are Free for Both Plans
 
 %It Is Calculated According to Plan 1
-amountOfPlan1=averageOfMonthlyCall*25; % Each Left Minutes Are Multiplied With 25 Kuruþ
-amountOfPlan1=amountOfPlan1/100; %Amount in Kuruþ is Converted to Turkish Lira
+amountOfPlan1=averageOfMonthlyCall*25; % Each Left Minutes Are Multiplied With 25 KuruÃ¾
+amountOfPlan1=amountOfPlan1/100; %Amount in KuruÃ¾ is Converted to Turkish Lira
 amountOfPlan1=amountOfPlan1+15; %Monthly Amount in Turkish Lira According to Plan 1
 
 fprintf("Monthly Amount According to Plan 1: %g TL \n",amountOfPlan1); % Monthly Amount According to Plan 1 Is Printed to the Screen 
 
 %It Is Calculated According to Plan 2
-amountOfPlan2=averageOfMonthlyCall*15; % Each Left Minutes Are Multiplied With 15 Kuruþ
-amountOfPlan2=amountOfPlan2/100; %Amount in Kuruþ is Converted to Turkish Lira 
+amountOfPlan2=averageOfMonthlyCall*15; % Each Left Minutes Are Multiplied With 15 KuruÃ¾
+amountOfPlan2=amountOfPlan2/100; %Amount in KuruÃ¾ is Converted to Turkish Lira 
 amountOfPlan2=amountOfPlan2+40; %Monthly Amount in Turkish Lira According to Plan 2
 
 fprintf("Monthly Amount According to Plan 2: %g TL \n",amountOfPlan2); % Monthly Amount According to Plan 2 Is Printed to the Screen
